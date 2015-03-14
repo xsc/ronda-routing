e81ff8ca69a0b3113fc0e20a7833bb4da368fc6f
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-03-14 17:55:06 +0100

    Version 0.2.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ronda/routing "0.2.1"
+(defproject ronda/routing "0.2.2-SNAPSHOT"
   :description "Middleware-based Routing Logic."
   :url "https://github.com/xsc/ronda-routing"
   :license {:name "Eclipse Public License"
