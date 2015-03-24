5b5fc7f347de29e7927a6bc3d0d81224af0a9c37
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-03-24 10:28:12 +0100

    include LICENSE URL.

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject ronda/routing "0.2.4-SNAPSHOT"
   :description "Middleware-based Routing Logic."
   :url "https://github.com/xsc/ronda-routing"
-  :license {:name "MIT License"}
+  :license {:name "MIT License"
+            :url "http://xsc.mit-license.org"}
   :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
                  [potemkin "0.3.12"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]
