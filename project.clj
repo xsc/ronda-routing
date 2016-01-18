b7fc8e837ce1d8d1e30caa047d2911a0ca1c2314
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-01-18 14:05:42 +0100

    Version 0.2.9-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ronda/routing "0.2.8"
+(defproject ronda/routing "0.2.9-SNAPSHOT"
   :description "Middleware-based Routing Logic."
   :url "https://github.com/xsc/ronda-routing"
   :license {:name "MIT License"
